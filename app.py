9ac9883df47259d5234bd8c13de8430c02ade606
camilotorresmestra <camilotorresmestra@gmail.com>
2026-01-13 18:42:35 -0500

    [REFactor] Remove unused PDF_BASE64 and OUTPUT_YAML variables

--- app.py
+++ app.py
@@ -4,25 +4,10 @@ import os
 from rendercv.api import create_a_pdf_from_a_yaml_string
 from rendercv.renderer.renderer import render_a_pdf_from_typst
 # Pre-baked PDF (base64) that will be shown on the right panel.
-PDF_BASE64 = (
-    "JVBERi0xLjQKJeLjz9MKMSAwIG9iago8PCAvVHlwZSAvQ2F0YWxvZyAvUGFnZXMgMiAwIFIg"
-    "Pj4KZW5kb2JqCjIgMCBvYmoKPDwgL1R5cGUgL1BhZ2VzIC9LaWRzIFszIDAgUl0gL0NvdW50"
-    "IDEgPj4KZW5kb2JqCjMgMCBvYmoKPDwgL1R5cGUgL1BhZ2UgL1BhcmVudCAyIDAgUiAvTWVk"
-    "aWFCb3ggWzAgMCA2MTIgNzkyXSAvQ29udGVudHMgNCAwIFIgL1Jlc291cmNlcyA8PCAvRm9u"
-    "dCA8PCAvRjEgNSAwIFIgPj4gPj4gPj4KZW5kb2JqCjQgMCBvYmoKPDwgL0xlbmd0aCA0MyA+"
-    "PgpzdHJlYW0KQlQKL0YxIDE4IFRmCjEwMCA3MDAgVGQKKEhlbGxvIFBERikgVGoKRVQKZW5k"
-    "c3RyZWFtCmVuZG9iago1IDAgb2JqCjwvVHlwZSAvRm9udCAvU3VidHlwZSAvVHlwZTEgL05h"
-    "bWUgL0YxIC9CYXNlRm9udCAvSGVsdmV0aWNhPj4KZW5kb2JqCnhyZWYKMCA2CjAwMDAwMDAw"
-    "MCA2NTUzNSBmIAowMDAwMDAwMTEgMDAwMDAgbiAKMDAwMDAwMDYwIDAwMDAwIG4gCjAwMDAw"
-    "MDA5OCAwMDAwMCBuIAowMDAwMDAxNzggMDAwMDAgbiAKMDAwMDAwMjk3IDAwMDAwIG4gCnRy"
-    "YWlsZXIKPDwgL1NpemUgNiAvUm9vdCAxIDAgUiAvSW5mbyA8PCAvQ3JlYXRvciAoRXhhbXBs"
-    "ZSkgPj4gPj4Kc3RhcnR4cmVmCjQ1MwolJUVPRgo="
-)
 
 import os
-import yaml
 
-OUTPUT_YAML = os.path.join(os.path.dirname(__file__), "input.yaml")
+
 
 
 def save_input_as_yaml(text: str, filename = 't.yaml') -> str:
